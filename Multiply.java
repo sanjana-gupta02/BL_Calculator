fd7d04eff96fb5ebe7a2143f5f1f68270538c03d
Sanjana Gupta <sanjanagupta1502@gmail.com>
2025-01-17 12:26:30 +0530

    added division code

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Multiply {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+        int multi = a*b;
+        System.out.println("Multiply of two numbers is: "+multi);
+    
+}
+}
